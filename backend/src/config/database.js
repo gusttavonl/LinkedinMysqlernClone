8ba83cd5fb01977a00ac0404b683150175f65a81
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-02-25 13:10:24 -0300

    Caminho database

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  port: '8111',
+  password: '',
+  database: 'linkedinMysqlernClone',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
