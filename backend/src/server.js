a87d954ee034299d78d5b1eb0115db217e3d30e2
Gustavo Noronha <77861206+GustavoNoronha0@users.noreply.github.com>
2021-03-02 15:19:46 -0300

    Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
+const cookieParser = require('cookie-parser'); // cookies no servidor
+const cors = require('cors'); // importando o cors
 
 require('./database');
 
 const app = express();
 
-app.use(express.json());
+app.use(cors()); // usando o app(express) com o cors(segurança dos dominios)
+app.use(cookieParser()); // usando o app(express) com o cookieParser
+app.use(express.json()); // para receber e usar json do back para o front
 app.use(routes);
 
-app.listen(3003);
+
+
+app.listen(3333);
